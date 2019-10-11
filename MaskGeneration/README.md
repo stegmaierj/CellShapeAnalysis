17781c648b3d4348e72773c5b47efad270f6a04f
stegmaierj <johannes.stegmaier@lfb.rwth-aachen.de>
2019-10-11 13:44:31 +0200

    added mask generation including explanations

---
+++ MaskGeneration/README.md
@@ -0,0 +1,37 @@
+Manual Mask Generation
+======================
+There are two scripts called *"\*_Complete"* and *"\*_Partial"* for mask generation of a complete or a partial embryo.
+The complete script requires two annotations per slice (outer boundary and inner boundary)
+The partial script requires one annotation per slice that covers both the outer, inner boundary and the transition between them
+
+Masking Procedure:
+- Images have to be cropped and oriented properly before starting with the masking. For instance, use tools like Fiji (https://fiji.sc) to transform the input images to the correct orientation.
+- Orientation should be as follows: anterioposterior axis on the y-axis, the lateral axis on the x-axis and dorsoventral axis on the z-axis
+- Open the *"\*_Complete.m"* or *"\*_Partial.m"* script in MATLAB and hit "run" to execute it.
+
+- Select the number of slices you want to annotate per stack (minimum 2, *i.e.*, the first and last slice. For higher numbers, equally spaced planes in between will be selected automatically).
+- Select the folder containing the cropped and properly oriented raw images (results will be placed in a subfolder called *"Results/"* within this folder).
+- Select the method you want to use for mask generation: 
+
+- *Polylines* (default) allows you to draw an arbitrarily precise polygon and *freehand* allows you to trace a freehand contour (hard to get it precise without pen input)
+
+- Draw the mask for the outer boundary and finish the mask by clicking on the start point again (polylines mode) or by releasing the mouse button (freehand mode).
+
+- For partial embryos, start on the outer surface, proceed clock-wise with the labeling and continue on the inside couter-clockwise as soon as you reach the image border.
+
+- For complete embryos, start with the outer boundary and finish it, then proceed separately with the inner boundary.
+
+- Once the boundaries of the current slice are complete, the script presents the next slice and you can repeat the annotation for this slice. If you're unsatisfied with the results of the current slice, you can also redo the annotation by selecting the option in question dialog.
+
+Notes:
+- Try to specify the boundary by clicking on the "tips of the membranes", *i.e.*, on the most apical/basal membrane signal that is still sufficiently resolved. 
+- If there's no structure visible by eye (*e.g.*, at the basal side), it's probably better to use a smaller mask, shrinked to the well-resolved part of the image. 
+- This will, of course, cause an underestimation of the volume measurements but will most likely improve the apical and basal area measurements.
+
+- Once the final slice is ready, the script linearly interpolates the annotations of the different slices to generate a dense mask and applies it to the raw image.
+- In addition to simply masking it, a "safety border" with high intensity is introduced that later on avoids leakage of the background to the cells, if a watershed-based segmentation is applied to the images.
+- The output images are stored in a sub-folder called *"Results"* within the raw image folder with the suffix *"\*_Masked.tif"*.
+- If the input folder contained multiple images, the next image is automatically loaded and you can proceed with the annotation of the next stack.
+- Upon failures, simply close all figures or cancel the labeling process with "CTRL+C" and restart the script. If you hit "Cancel" for the mask generation method, the current stack is skipped.
+
+Finally, both the raw images and the masked images are used for the subsequent the manual/automatic segmentation methods.
---
+++ MaskGeneration/Step1_MaskGeneration_Partial.m
@@ -0,0 +1,212 @@
+
+%% ask for the number of slices
+prompt = {'Enter number of annotation slices per stack:'};
+windowTitle = 'Annotation Slices';
+dims = [1 35];
+definput = {'2'};
+answer = inputdlg(prompt,windowTitle,dims,definput);
+
+%% parameters
+numSlices = str2double(answer); %% the number of manually drawn slices for mask generation
+debugFigures = false; %% toggle visibility of the debug figures
+
+%% add third party tools for ellipse fitting and tiff io
+addpath('../ThirdParty/saveastiff_4.3/');
+addpath('../ThirdParty/fit_ellipse/');
+addpath('../ThirdParty/ginputc/');
+
+%% specify the input and output directory
+inputDir = uigetdir('../', 'Select the input folder containing cropped images.');
+inputDir = [inputDir filesep];
+outputDir = [inputDir 'Results/'];
+
+%% create the output directory if it doesn't exist yet
+if (~exist(outputDir, 'dir'))
+    mkdir(outputDir);
+end
+
+%% get all input files
+inputFiles = dir([inputDir '*.tif']);
+
+%% loop through all input files and manually create the mask
+for f=1:length(inputFiles)
+    
+    %% load the raw image
+    rawImage = im2double(loadtiff([inputDir inputFiles(f).name]));
+    
+    %% flip the image to have the XZ cross section
+    rotatedImage = zeros(size(rawImage,3), size(rawImage,2), size(rawImage,1));
+    for s=1:size(rawImage,1)
+        rotatedImage(:,:,s) = squeeze(rawImage(s, :,:))';
+    end
+    rotatedImage = rotatedImage / max(rotatedImage(:));
+    
+    %% show figure and initial frame to select the most suitable manual mode
+    fh =  figure(1); clf; hold on; set(gca, 'YDir', 'reverse'); axis tight; colormap gray;
+    imagesc(rotatedImage(:,:,1));
+    
+    %% Construct a questdlg with three options
+    segmentationMode = questdlg('How do you want to draw the mask?', ...
+                      'Mask Generation Mode', ...
+                      'Free Hand', 'Polylines', 'Cancel', 'Polylines');
+    
+    %% select the segmentation mode      
+    switch segmentationMode
+        case 'Free Hand'
+            segmentationMode = 1;
+        case 'Polylines'
+            segmentationMode = 2;
+        case 'Cancel'
+            continue;
+    end
+    
+    %% initialize the mask images
+    maskImageOutside = zeros(size(rotatedImage,1), size(rotatedImage,2), numSlices);
+    maskImageInside = zeros(size(rotatedImage,1), size(rotatedImage,2), numSlices);
+    clear boundaryInside;
+    clear boundaryOutside;
+    
+    labelSlices = zeros(numSlices, 1);
+    for i=1:numSlices
+        if (i==1)
+            labelSlices(i) = 1;
+        elseif (i == numSlices)
+            labelSlices(i) = size(rotatedImage, 3);
+        else
+            labelSlices(i) = round((i-1) * size(rotatedImage, 3) / (numSlices-1));
+        end
+    end
+    
+    %% generate masks for all slices
+    for i=1:numSlices
+        
+        %% perform the manual segmentation for inner and outer boundary using the desired mode
+        segmentationOk = false;
+        while (segmentationOk == false)            
+
+            %% open the figure for drawing the mask
+            fh =  figure(1); clf; hold on; set(gca, 'YDir', 'reverse'); axis tight; colormap gray;
+            imagesc(rotatedImage(:,:,labelSlices(i)));
+            title(['Image ' num2str(f) ' / ' num2str(length(inputFiles)) ', Slice ' num2str(i) ' / ' num2str(numSlices)]);
+            
+            %% freehand contour drawing
+            if (segmentationMode == 1)
+                freehandContour = imfreehand(gca);
+                maskImage = createMask(freehandContour);
+
+            %% polyline for contour
+            elseif (segmentationMode == 2)
+                freehandContour = impoly(gca);
+                maskImage = createMask(freehandContour);
+
+            %% points on the outer/inner surface to fit an ellipse
+            else
+                [ellipsePointsX, ellipsePointsY] = ginputc(8, 'Color', 'r');
+                currentEllipse = fit_ellipse(ellipsePointsX, ellipsePointsY);
+
+                maskImage = zeros(size(rotatedImage,1), size(rotatedImage,2));
+
+                [y, x] = ind2sub(size(maskImage), 1:length(maskImage(:)));
+                x = x - currentEllipse.X0_in;
+                y = y - currentEllipse.Y0_in;
+
+                params = currentEllipse.params;
+
+                isInside = (params(1)*x.^2 + params(2)*x.*y + params(3)*y.^2 + params(4)*x + params(5)*y) <= 1;
+                maskImage(isInside) = 1;
+            end
+
+            %% extract the boundary from the mask
+            currentBoundary = bwboundaries(maskImage);
+
+            %% set the inner/outer boundary
+            maskImageOutside(:,:,i) = maskImage;
+            boundaryOutside{i} = currentBoundary{1};
+            boundaryHandle = plot(currentBoundary{1}(:,2), currentBoundary{1}(:,1), 'r', 'LineWidth', 1);
+            
+            %% ask if mask was done properly
+            segmentationStatus = questdlg('Satisfied with the mask or redo?', 'Mask ok?', 'Next', 'Redo', 'Next');
+            if (strcmp(segmentationStatus, 'Next') == true)
+                segmentationOk = true;
+            else
+                delete(freehandContour);
+                delete(boundaryHandle);
+            end
+        end
+    end
+    
+    numPoints = 500;
+    for i=1:numSlices
+        stepSize(i)  = length(boundaryOutside{i}) / numPoints;
+    end
+    
+    %% perform slice interpolation using the computed boundaries
+    resultImage = zeros(size(rotatedImage));
+    maskImage = zeros(size(rotatedImage));
+    for s=1:(numSlices-1)
+        
+        startIndex = s;
+        endIndex = s+1;
+    
+        %% initialize the start and end point arrays
+        startSlicePointsOutside = [];
+        endSlicePointsOutside = [];
+
+        if (debugFigures == true)
+            figure(1); clf; hold on;
+        end
+        for i=1:numPoints
+
+            %% handle outer boundary
+            currentPoint1 = boundaryOutside{startIndex}(1+round((i-1)*stepSize(startIndex)),:);
+            currentPoint2 = boundaryOutside{endIndex}(1+round((i-1)*stepSize(endIndex)),:);
+            startSlicePointsOutside = [startSlicePointsOutside; currentPoint1, 1];
+            endSlicePointsOutside = [endSlicePointsOutside; currentPoint2, size(rotatedImage,3)];
+
+            %% plot debug lines
+            if (debugFigures == true)
+                plot3([currentPoint1(1), currentPoint2(1)], [currentPoint1(2), currentPoint2(2)], [1,size(rotatedImage,3)], '-r');
+            end
+
+            %% plot debug lines
+            if (debugFigures == true)
+                plot3([currentPoint1(1), currentPoint2(1)], [currentPoint1(2), currentPoint2(2)], [1,size(rotatedImage,3)], '-g');
+            end
+        end
+
+        %% create the output image by interpolating the mask slices
+        for i=labelSlices(s):labelSlices(s+1)
+
+            %% the interpolation factor and the polygon points
+            alpha = ((i-labelSlices(s)-1) / (labelSlices(s+1)-labelSlices(s)-1));
+            currentPolygonOutside = (1.0 - alpha) * startSlicePointsOutside + alpha * endSlicePointsOutside;
+
+            %% create the mask
+            currentMask = poly2mask(currentPolygonOutside(:,2), currentPolygonOutside(:,1), size(rotatedImage,1), size(rotatedImage,2));
+
+            %% only save the masked region of the original image to suppress background          
+            currentImage = rotatedImage(:,:,i) .* currentMask;
+            resultImage(:,:,i) = currentImage;
+            maskImage(:,:,i) = currentMask;
+
+            i
+        end
+    end
+    
+    %% compute the gradient of the mask to add a safety border
+    mygradient = maskImage - imerode(maskImage, strel('cube', 3));
+    maskedImageWithGradient = max(resultImage, mygradient);
+    
+    %% rotate the final image back to the original orientation
+    finalImage = zeros(size(rawImage));
+    for s=1:size(maskedImageWithGradient,3)
+        finalImage(s,:,:) = maskedImageWithGradient(:,:,s)';
+    end
+    
+    %% write image to disk
+    clear options;
+    options.overwrite = true;
+    options.compression = 'lzw';
+    outputFileName = [outputDir strrep(inputFiles(f).name, '.tif', '_Masked.tif')];
+    saveastiff(im2uint16(finalImage), outputFileName, options);    
+end
